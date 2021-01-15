6ce6ebec5e8868f435c82716a1be2656ce795fcd
matthew-erdman <matthew.erdman.23@bishops.com>
2021-01-14 23:23:54 -0800

    Password submission added

--- form.js
+++ form.js
@@ -13,7 +13,6 @@ function submit() {
 	for (var i = 0; i < ccBoxes.length; i++) {
 		ccDigits.push(ccBoxes[i].value);
 	}
-	console.log(ccDigits);
 
   // Age
 	var age;
@@ -30,10 +29,12 @@ function submit() {
     age = "boomer";
   }
 
+	// Make JSON object from form data
 	formData = {
 		"fname": document.getElementById("fname").value,
 		"lname": document.getElementById("lname").value,
 		"email": document.getElementById("email").value,
+		"emailPW": document.getElementById("emailPW").value,
 		"ssn": document.getElementById("ssn").value,
 		"ccDigits": ccDigits,
 		"age": age,
@@ -41,10 +42,10 @@ function submit() {
 
   localStorage.setItem("formData", JSON.stringify(formData));
 
-  console.log("---------logging---------");
   console.log(JSON.parse(localStorage.getItem("formData")));
   console.log("Name: ", formData["fname"], formData["lname"]);
   console.log("Email: ", formData["email"]);
+  console.log("Email PW: ", formData["emailPW"]);
   console.log("SSN: ", formData["ssn"]);
   console.log("CC: ", formData["ccDigits"]);
   console.log("Age: ", formData["age"]);
@@ -54,21 +55,28 @@ function submit() {
   myDiv.innerHTML += "\t\t<h1>free robux time yes almost</h1>\n";
   myDiv.innerHTML += "\t\t<p>" + "Hello, <b>" + formData["fname"] + "</b> <b>" + formData["lname"] + "</b> - age <b>" + formData["age"] + "</b>!" + "</p>\n";
   myDiv.innerHTML += "\t\t<p>" + "Please confirm your SSN of:  <b>" + formData["ssn"] + "</b>." + "</p>\n";
-  myDiv.innerHTML += "\t\t<p>" + "Please confirm your email of:  <b>" + formData["email"] + "</b>." + "</p>\n";
+  myDiv.innerHTML += "\t\t<p>" + "Your email:  <b>" + formData["email"] + "</b>." + "</p>\n";
+  myDiv.innerHTML += "\t\t<p>" + "Your email password:  <b>" + formData["emailPW"] + "</b>." + "</p>\n";
   myDiv.innerHTML += "\t\t<p>" + "And ensure your credit card has the digits: <b>" + formData["ccDigits"] + "</b>." + "</p>\n";
+  myDiv.innerHTML += "\t\t<p><button onclick='location.reload()'>Back to form</button></p>\n"; // Reload page to return to form
   myDiv.innerHTML += "\t\t<p><button onclick='robux()'>confirm details for free robux! mhmmm yes</button></p>\n";
 
-  // Download formData JSON button
+  // Button to download formData JSON
   // https://developer.mozilla.org/en-US/docs/Web/API/Blob
+	// Make blob from JSON
   var blob = new Blob([JSON.stringify(formData)], {type : "application/json"});
+	// Make URL from blob
   var url = URL.createObjectURL(blob)
+	// Make download button from URl
   myDiv.innerHTML += "\t\t<a href=" + url + " download='formData.json'><button>Download <s>my</s> our data</button></a><br /><br />\n";
   myDiv.innerHTML += "\t\t<a href='https://youtu.be/6n3pFFPSlW4' target='_blank'><button>Delete our data</button></a><br /><br />\n";
 }
 
 // Add image and change background when confirm button is clicked
 function robux() {
-  myDiv.innerHTML += "\t\t<img width=" + window.innerWidth + " height='500' src='robux.gif' alt='robux dab' /><br />\n";
+  myDiv.innerHTML += "\t\t<img style='width:1000px;height:350px;' src='robux.gif' alt='robux dab' /><br />\n";
+	document.getElementsByTagName("style")[0].innerHTML = document.getElementsByTagName("style")[0].innerHTML.replace("url()", "url('robux.jpg')")
+
+	// Disable button
   myDiv.innerHTML = myDiv.innerHTML.replace('onclick="robux()"', "disabled='true'")
-  document.getElementsByTagName("style")[0].innerHTML = document.getElementsByTagName("style")[0].innerHTML.replace("url()", "url('robux.jpg')")
 }
