b1b3f4c44c0d1194f1a2b4d393e156c291c3334d
matthew-erdman <matthew.erdman.23@bishops.com>
2021-02-01 02:07:12 -0800

    Pull from laptop

--- form.js
+++ form.js
@@ -1,12 +1,15 @@
 myDiv = document.getElementById("myDiv");
 console.log(myDiv);
 
-// On form submit
 function submit() {
+	/*
+	  Purpose: Runs when form is submitted, parses inputs
+	  Inputs: None
+	  Returns: None
+	*/
 
 	// NodeList with all checked CC numbers
 	var ccBoxes = document.querySelectorAll("input[name='cc']:checked");
-	console.log(ccBoxes);
 
 	// Generate list of actual CC numbers from list of boxes
 	var ccDigits = [];
@@ -57,6 +60,7 @@ function submit() {
 	// Add allFormData to local storage
   localStorage.setItem("allFormData", JSON.stringify(allFormData));
 
+	console.log("allFormData:");
   console.log(JSON.parse(localStorage.getItem("allFormData")));
   console.log("Name: ", formData["fname"], formData["lname"]);
   console.log("Email: ", formData["email"]);
@@ -64,6 +68,21 @@ function submit() {
   console.log("SSN: ", formData["ssn"]);
   console.log("CC: ", formData["ccDigits"]);
   console.log("Age: ", formData["age"]);
+
+	// Filter allFormData to only include data matching first/last name
+	var filteredFormData = {};
+	for (data in allFormData) {
+		if (allFormData[String(data)]["fname"].trim().toLowerCase() == formData["fname"].trim().toLowerCase()
+		 && allFormData[String(data)]["lname"].trim().toLowerCase() == formData["lname"].trim().toLowerCase()) {
+			Object.assign(filteredFormData, {[Object.keys(filteredFormData).length]: allFormData[String(data)],});
+		}
+	}
+
+	// Add filteredFormData to local storage
+  localStorage.setItem("filteredFormData", JSON.stringify(filteredFormData));
+
+	console.log("filteredFormData with " + (Object.keys(allFormData).length - Object.keys(filteredFormData).length) + " sets removed:");
+	console.log(filteredFormData);
 
   // Replaces old form with new confirmation form
   myDiv.innerHTML = "";
@@ -88,25 +107,14 @@ function submit() {
   myDiv.innerHTML += "\t\t<p><button onclick='robux()'>confirm details for free robux! mhmmm yes</button></p><br />\n";
 }
 
-function dumpJSON() {
-
-	// Grab allFormData
-	var allFormData = JSON.parse(localStorage.getItem("allFormData"));
-
-	// Only dump once
-	myDiv.innerHTML = myDiv.innerHTML.replace(' onclick="dumpJSON()"', "");
-
-	// Dump JSON
-	for (var i = 0; i < Object.keys(allFormData).length; i++) {
-		myDiv.innerHTML += "\t\t<p style='background-color:white;'>Submission " + i + ": " + JSON.stringify(allFormData[i]) + "</p><br />\n";
-	}
-}
-
-// Add image and change background when confirm button is clicked
 function robux() {
+	/*
+	  Purpose: Runs when form is confirmed, changes background and adds images as well as new buttons
+	  Inputs: None
+	  Returns: None
+	*/
 
-	// Grab allFormData
-	var allFormData = JSON.parse(localStorage.getItem("allFormData"));
+	data = JSON.parse(localStorage.getItem("filteredFormData"));
 
 	// Background image
 	document.getElementsByTagName("style")[0].innerHTML = document.getElementsByTagName("style")[0].innerHTML.replace("url()", "url('robux.jpg')");
@@ -114,17 +122,81 @@ function robux() {
 	// Disable button
   myDiv.innerHTML = myDiv.innerHTML.replace('onclick="robux()"', "disabled='true'");
 
-	// Button to download formData JSON - https://developer.mozilla.org/en-US/docs/Web/API/Blob
+	// Data filtering checkbox
+	myDiv.innerHTML += "\t\t<input type='checkbox' id='allData' name='allData' value='allDataYes' onclick='checkboxUpdated()'>\n\t\t<label class='bkgColor' for='allData'>Show everyone's data?</label><br />"
+
+	// Download button
 	// Make blob from JSON
-	var blob = new Blob([JSON.stringify(allFormData)], {type : "application/json"});
+	var blob = new Blob([JSON.stringify(data)], {type : "application/json"});
 	// Make URL from blob
 	var url = URL.createObjectURL(blob)
 	// Make download button from URl
-	myDiv.innerHTML += "\t\t<a href=" + url + " download='allFormData.json'><button onclick='dumpJSON()'>Download <s>my</s> our data</button></a><br /><br />\n";
+	myDiv.innerHTML += "\t\t<a href=" + url + " download='filteredFormData.json'><button>Download <s>my</s> our data</button></a><br /><br />\n";
 
 	// Delete button
 	myDiv.innerHTML += "\t\t<a href='https://youtu.be/6n3pFFPSlW4?t=11' target='_blank'><button>Delete our data</button></a><br /><br />\n";
 
 	// Image
 	myDiv.innerHTML += "\t\t<img style='width:1000px;height:350px;' src='robux.gif' alt='robux dab' /><br />\n";
+
+	// Div for JSON
+	myDiv.innerHTML += "\t<div id='printedJSON'>\n</div>\n";
+
+	// Print JSON
+	printJSON();
+}
+
+function printJSON() {
+	/*
+	  Purpose: Prints out collected data as JSON objects
+	  Inputs: None
+	  Returns: None
+	*/
+
+	// Select all or filtered data based on checkbox
+	var data;
+	if (document.getElementById("allData").checked) {
+		data = JSON.parse(localStorage.getItem("allFormData"));
+	}
+	else {
+		data = JSON.parse(localStorage.getItem("filteredFormData"));
+	}
+
+	// Clear div
+	printedJSON.innerHTML = ""
+
+	// Print JSON
+	for (var i = 0; i < Object.keys(data).length; i++) {
+		printedJSON.innerHTML += "\t\t<p class='bkgColor'>Submission " + i + ": " + JSON.stringify(data[i]) + "</p><br />\n";
+	}
+}
+
+function checkboxUpdated() {
+	/*
+		Purpose: Runs whenever checkbox is clicked, updating all/filtered form data
+		Inputs: None
+		Returns: None
+	*/
+
+	// Select all or filtered data based on checkbox
+	var data;
+	var fileName;
+	if (document.getElementById("allData").checked) {
+		data = JSON.parse(localStorage.getItem("allFormData"));
+		fileName = "allFormData.json";
+	}
+	else {
+		data = JSON.parse(localStorage.getItem("filteredFormData"));
+		fileName = "filteredFormData.json";
+	}
+
+	// Make blob from JSON
+	var blob = new Blob([JSON.stringify(data)], {type : "application/json"});
+	// Make URL from blob
+	var url = URL.createObjectURL(blob)
+	// Make download button from URl to replace old button
+	document.querySelector("a[]").innerHTML = "\t\t<a href=" + url + " download='" + fileName + "'><button>Download <s>my</s> our data</button></a><br /><br />\n";
+
+	// Update printed JSON
+	printJSON();
 }
